f1e2f01c1cadb88a8d03b3c3abb4b3fffe7b4d6f
Skylar Gayhart <55904203+sgayha1@users.noreply.github.com>
2020-06-17 16:19:59 -0400

    Update Exercise 1.py

--- PracticePython.org/Exercise 1.py
+++ PracticePython.org/Exercise 1.py
@@ -1,6 +1,8 @@
 # Create a program that asks the user to enter their name and their age. Print out a message addressed to them that
-# tells them the year that they will turn 100 years old.
+# tells them the year that they will turn 100 years old. Add on to the previous program by asking the user for 
+# another number and printing out that many copies of the previous message.
 
+# Instatiation of a count variable
 count = 0
 
 # Get user input
